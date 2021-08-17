0d2d526dc5b49aafa3511d003e4c6c8f431b56aa
rajesh <rajesh.sen@customerxps.com>
2021-08-17 19:16:41 +0530

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'clari5-ui'
+import 'clari5-ui/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
